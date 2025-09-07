f75965088d4ecf973465c150d0c8c92565e72c28
StrJosedavi <josedavi100@gmail.com>
2025-09-07 02:50:56 -0300

    feature: restructure inventory sorting tests for clarity and organization

--- cypress/e2e/inventory/inventory.cy.ts
+++ cypress/e2e/inventory/inventory.cy.ts
@@ -9,47 +9,53 @@ describe('Feature: Viewing, detailing and initializing product pre-sales', () =>
         cy.loginValid()
     })
 
-    /*
-        O caso de teste tem como objetivo validar a lista de produtos ordenadas do menor para o maior preço,
-        garantindo que os produtos são exibidos corretamente conforme o critério de ordenação selecionado.
-    */
-    it('Scenario: View product list sorted by price low to high', () => {   
-        // Ordenar do menor para o maior preço
-        inventory.selectSort('lohi')
-        
-        inventory.assertFilterPrice(true)
+    describe('sort by price', () => {
+
+        /*
+            O caso de teste tem como objetivo validar a lista de produtos ordenadas do menor para o maior preço,
+            garantindo que os produtos são exibidos corretamente conforme o critério de ordenação selecionado.
+        */
+        it('Scenario: View product list sorted by price low to high', () => {
+            // Ordenar do menor para o maior preço
+            inventory.selectSort('lohi')
+
+            inventory.assertFilterPrice(true)
+        })
+
+        /*
+            O caso de teste tem como objetivo validar a lista de produtos ordenada do maior para o menor preço,
+            garantindo que os produtos são exibidos corretamente conforme o critério de ordenação selecionado.
+        */
+        it('Scenario: View product list sorted by price high to low', () => {
+            // Ordenar do maior para o menor preço
+            inventory.selectSort('hilo')
+
+            inventory.assertFilterPrice(false)
+        })
     })
 
-    /*
-        O caso de teste tem como objetivo validar a lista de produtos ordenada do maior para o menor preço,
-        garantindo que os produtos são exibidos corretamente conforme o critério de ordenação selecionado.
-    */
-    it('Scenario: View product list sorted by price high to low', () => {   
-        // Ordenar do maior para o menor preço
-        inventory.selectSort('hilo')
 
-        inventory.assertFilterPrice(false)
-    })
+    describe('sort by name', () => {
+        /*
+            O caso de teste tem como objetivo validar a lista de produtos ordenada em ordem alfabética crescente (A → Z),
+            garantindo que os produtos são exibidos corretamente conforme o critério de ordenação selecionado.
+        */
+        it('Scenario: View product list sorted by name A to Z', () => {
+            // Ordenar de A para Z
+            inventory.selectSort('az')
 
-    /*
-        O caso de teste tem como objetivo validar a lista de produtos ordenada em ordem alfabética crescente (A → Z),
-        garantindo que os produtos são exibidos corretamente conforme o critério de ordenação selecionado.
-    */
-    it('Scenario: View product list sorted by name A to Z', () => {   
-        // Ordenar de A para Z
-        inventory.selectSort('az')
-        
-        inventory.assertFilterName(true) 
-    })
+            inventory.assertFilterName(true)
+        })
 
-    /*
-        O caso de teste tem como objetivo validar a lista de produtos ordenada em ordem alfabética decrescente (Z → A),
-        garantindo que os produtos são exibidos corretamente conforme o critério de ordenação selecionado.
-    */
-    it('Scenario: View product list sorted by name Z to A', () => {   
-        // Ordenar de Z para A
-        inventory.selectSort('za')
+        /*
+            O caso de teste tem como objetivo validar a lista de produtos ordenada em ordem alfabética decrescente (Z → A),
+            garantindo que os produtos são exibidos corretamente conforme o critério de ordenação selecionado.
+        */
+        it('Scenario: View product list sorted by name Z to A', () => {
+            // Ordenar de Z para A
+            inventory.selectSort('za')
 
-        inventory.assertFilterName(false)
+            inventory.assertFilterName(false)
+        })
     })
 })
