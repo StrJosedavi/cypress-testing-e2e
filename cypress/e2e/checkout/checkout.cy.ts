4c5e8a095e293fd919a2448cd3187a8539532ba3
StrJosedavi <josedavi100@gmail.com>
2025-09-07 12:47:33 -0300

    feature: enhance checkout flow with additional validation tests and improved assertions

--- cypress/e2e/checkout/checkout.cy.ts
+++ cypress/e2e/checkout/checkout.cy.ts
@@ -13,33 +13,59 @@ const completeCheckoutPage = new CompleteCheckoutPage()
 describe('Feature: Product selection and order completion', () => {
     beforeEach(() => {
         cy.loginValid()
-    })
 
-    /*
-        O caso de teste tem como objetivo validar o fluxo de compra, desde a adição do produto ao carrinho
-        até onde o usuário consegue avançar para a página de checkout.
-    */
-    it('Scenario: Add product to cart and advance to checkout', () => {   
         inventory.addItemByIndex(0)
         inventory.goToCart()
         cart.proceedToCheckout()
-
-        step1Checkout.assertPageStep1CheckoutIsVisible()
     })
 
-    /*
-        O caso de teste tem como objetivo validar o fluxo de compra, desde a adição do produto ao carrinho
-        até a finalização da compra, garantindo que o usuário consiga avançar para a página de checkout e finalizar o pedido.
-    */
-    it('Scenario: Add product to cart and advance to checkout', () => {   
-        inventory.addItemByIndex(0)
-        inventory.goToCart()
-        cart.proceedToCheckout()
+    describe('checkout step one', () => {
+        /*
+            O caso de teste tem como objetivo validar o fluxo de compra, desde a adição do produto ao carrinho
+            até onde o usuário consegue avançar para a página de checkout.
+        */
+        it('Scenario: Add product to cart and advance to checkout', () => {
+            step1Checkout.assertPageStep1CheckoutIsVisible()
+        })
 
-        step1Checkout.fillCheckoutInformation('Jose', 'Silva', '12345')
-        step1Checkout.clickButtonContinue()
-        step2Checkout.clickButtonFinish()
+        /*
+            O caso de teste tem como objetivo validar o fluxo de compra, desde a adição do produto ao carrinho
+            até a finalização da compra, garantindo que o usuário consiga avançar para a página de checkout e finalizar o pedido.
+        */
+        it('Scenario: Add product to cart and advance to checkout', () => {
+            step1Checkout.fillCheckoutInformation('Jose', 'Silva', '12345')
+            step1Checkout.clickButtonContinue()
+            step2Checkout.clickButtonFinish()
 
-        completeCheckoutPage.assertPageCompleteCheckoutIsVisible()
+            completeCheckoutPage.assertPageCompleteCheckoutIsVisible()
+        })
+
+        /*
+            O caso de teste tem como objetivo validar as validações do formulário de checkout na etapa 1,
+            garantindo que o usuário receba mensagens de erro apropriadas ao tentar avançar sem preencher os campos obrigatórios.
+        */
+        it('Scenario: Add product to cart and advance to checkout and not fill the fields of step one', () => {
+            step1Checkout.clickButtonContinue()
+            step1Checkout.assertErrorMessageIsVisible('firstName')
+
+            step1Checkout.fillCheckoutInformation('Jose')
+            step1Checkout.clickButtonContinue()
+            step1Checkout.assertErrorMessageIsVisible('lastName')
+
+            step1Checkout.fillCheckoutInformation('Jose', 'Silva')
+            step1Checkout.clickButtonContinue()
+            step1Checkout.assertErrorMessageIsVisible('postalCode')
+        })
     })
+    describe('checkout step two', () => {
+         /*
+            O caso de teste tem como objetivo validar o fluxo de compra, desde a adição do produto ao carrinho
+            até onde o usuário consegue avançar para a página de checkout.
+        */
+        it('Scenario: Add product to cart and advance to checkout', () => {
+            step1Checkout.assertPageStep1CheckoutIsVisible()
+        })
+
+    })
+
 })
--- cypress/pages/step-1-checkout.page.ts
+++ cypress/pages/step-1-checkout.page.ts
@@ -1,29 +1,50 @@
 export class Step1CheckoutPage {
 
-    private readonly inputFirstName = '#first-name'
-    private readonly inputLastName = '#last-name'
-    private readonly inputPostalCode = '#postal-code'
-    private readonly buttonContinue = '//input[@value="CONTINUE"]'
+  private readonly inputFirstName = '#first-name'
+  private readonly inputLastName = '#last-name'
+  private readonly inputPostalCode = '#postal-code'
+  private readonly buttonContinue = '//input[@value="CONTINUE"]'
 
-    visit() {
-        cy.visit('/checkout-step-one.html')
+  visit() {
+    cy.visit('/checkout-step-one.html')
+  }
+
+  clickButtonContinue() {
+    cy.xpath(this.buttonContinue).click()
+  }
+
+  // Fluxos 
+  fillCheckoutInformation(firstName?: string, lastName?: string, postalCode?: string) {
+    if (firstName) {
+      cy.get(this.inputFirstName).clear().type(firstName)
     }
 
-    clickButtonContinue() {
-      cy.xpath(this.buttonContinue).click()
-    }
-    
-    // Fluxos 
-    fillCheckoutInformation(firstName: string = '', lastName: string = '', postalCode: string = '') {
-      cy.get(this.inputFirstName).type(firstName)
-      cy.get(this.inputLastName).type(lastName)
-      cy.get(this.inputPostalCode).type(postalCode)
+    if (lastName) {
+      cy.get(this.inputLastName).clear().type(lastName)
     }
 
-    // Asserções
-    assertPageStep1CheckoutIsVisible() {
-      cy.get(this.inputFirstName).should('be.visible')
-      cy.get(this.inputLastName).should('be.visible')
-      cy.get(this.inputPostalCode).should('be.visible')
+    if (postalCode) {
+      cy.get(this.inputPostalCode).clear().type(postalCode)
     }
+  }
+
+
+  // Asserções
+  assertPageStep1CheckoutIsVisible() {
+    cy.get(this.inputFirstName).should('be.visible')
+    cy.get(this.inputLastName).should('be.visible')
+    cy.get(this.inputPostalCode).should('be.visible')
+  }
+
+  assertErrorMessageIsVisible(field: string) {
+    cy.get('form h3[data-test="error"]').should('be.visible')
+
+    if (field === 'firstName') {
+      cy.get('form h3[data-test="error"]').should('have.text', 'Error: First Name is required')
+    } else if (field === 'lastName') {
+      cy.get('form h3[data-test="error"]').should('have.text', 'Error: Last Name is required')
+    } else if (field === 'postalCode') {
+      cy.get('form h3[data-test="error"]').should('have.text', 'Error: Postal Code is required')
+    }
+  }
 }
